b65f72e2ad49d5d8c85fc3dbf67bf0f106208f01
FelipeImhoff <felipe_imhoff@hotmail.com>
2023-09-23 15:16:03 +0000

    Adicionada funcao para alterar um esporte

--- src/controllers/EsporteController.ts
+++ src/controllers/EsporteController.ts
@@ -2,35 +2,58 @@ import { Request, Response } from "express";
 import { prismaClient } from "../database/prismaClient";
 
 export class EsporteController {
-    async index(request: Request, response: Response){
-        const data = await prismaClient.esporte.findMany();
-        
-        if(!data.length){
-            return response.status(404).json("No esportes found")
-        }
-        return response.json(data)
+  async index(request: Request, response: Response) {
+    const data = await prismaClient.esporte.findMany();
+
+    if (!data.length) {
+      return response.status(404).json("Nenhum esporte encontrado");
     }
-    
-    async store(request: Request, response: Response){
-        const { nome, descricao } = request.body;
-        const data = await prismaClient.esporte.create({
-            data: {
-                nome,
-                descricao
-            }
-        })
-        
-        return response.status(201).json(data)
+    return response.json(data);
+  }
+
+  async store(request: Request, response: Response) {
+    const { nome, descricao } = request.body;
+
+    if (nome || descricao) {
+      return response.status(400).json({
+        Erro: "Parâmetros ausentes",
+        Mensagem:
+          "Alguns parâmetros obrigatórios estão ausentes na sua solicitação. Certifique-se de incluir todos os parâmetros necessários para a gravação de dados.",
+      });
     }
-    
-    async show(request: Request, response: Response){
-        const {id} =  request.params;
-        const data = await prismaClient.esporte.findUnique({
-            where: {
-                id
-            }
-        })
-        
-        return response.json(data)
-    }
+
+    const data = await prismaClient.esporte.create({
+      data: {
+        nome,
+        descricao,
+      },
+    });
+
+    return response.status(201).json(data);
+  }
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+    const data = await prismaClient.esporte.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return response.json(data);
+  }
+
+  async update(request: Request, response: Response) {
+    const { id } = request.params;
+    const body = request.body;
+
+    const data = await prismaClient.esporte.update({
+      where: {
+        id,
+      },
+      data: body,
+    });
+
+    return response.json(data);
+  }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { EsporteController } from "./controllers/EsporteController";
 
 const router = Router();
-const esporteController = new EsporteController()
+const esporteController = new EsporteController();
 
 const esporteRouter = Router();
-esporteRouter.get('/', esporteController.index);
-esporteRouter.post('/', esporteController.store);
-esporteRouter.get('/:id', esporteController.show);
+esporteRouter.get("/", esporteController.index);
+esporteRouter.post("/", esporteController.store);
+esporteRouter.get("/:id", esporteController.show);
+esporteRouter.put("/:id", esporteController.update);
 
 //outras rotas aqui
 
