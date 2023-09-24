5f9e060479ed292a5f99c4674359315826513bc3
FelipeImhoff <felipe_imhoff@hotmail.com>
2023-09-24 16:44:32 +0000

    Criado CRUD do campeonato

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { EsporteController } from "./controllers/EsporteController";
 import { EquipeController } from "./controllers/EquipeController";
+import { CampeonatoController } from "./controllers/CampeonatoController";
 
 const router = Router();
 const esporteController = new EsporteController();
 const equipeController = new EquipeController();
+const campeonatoController = new CampeonatoController();
 
 const esporteRouter = Router();
 esporteRouter.get("/", esporteController.index);
@@ -20,8 +22,16 @@ equipeRouter.get("/:id", equipeController.show);
 equipeRouter.put("/:id", equipeController.update);
 equipeRouter.delete("/:id", equipeController.destroy);
 
+const campeonatoRouter = Router();
+campeonatoRouter.get("/", campeonatoController.index);
+campeonatoRouter.post("/", campeonatoController.store);
+campeonatoRouter.get("/:id", campeonatoController.show);
+campeonatoRouter.put("/:id", campeonatoController.update);
+campeonatoRouter.delete("/:id", campeonatoController.destroy);
+
 //outras rotas aqui
 router.use("/api/esporte", esporteRouter);
 router.use("/api/equipe", equipeRouter);
+router.use("/api/campeonato", campeonatoRouter);
 
 export { router };
